4d35941f583d50c5d7fe1e24e952bb1084749802
ernestopi <90855907+ernestopi@users.noreply.github.com>
2022-03-28 20:21:07 -0300

    Add files via upload

---
+++ SEM-04-T1-Q04 quantos segundos se passaram.py
@@ -0,0 +1,6 @@
+hora = int(input())
+minutos = int(input())
+segundos = int(input())
+minutos = (hora*60)+minutos
+segundos = (minutos*60)+segundos
+print(segundos)
---
+++ SEM-04-T1-Q05 Area volume.py
@@ -0,0 +1,9 @@
+altura = float(input())
+largura = float(input())
+comprimento = float(input())
+area_piso = largura*comprimento
+vol_da_sala = largura*comprimento*altura
+area_das_paredes = 2*altura*largura+2*altura*comprimento
+print(area_piso)
+print(vol_da_sala)
+print(area_das_paredes)
